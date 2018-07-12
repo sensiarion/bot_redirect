d079f5715edd574915eda08b4b5978276eb21ccd
Mansur Izert <izertmi@gmail.com>
2018-07-12 23:42:57 +0500

    redirect

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+import json
 
+from flask import Flask
+from flask import request
+import requests
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
     return "HEY NIGGA!"
+
+
+@app.route("/url", methods=['POST'])
+def redirect():
+    if request.is_json:
+        print(request)
+        url = request.get_json()['url']
+        response = requests.get(url)
+        print(response.status_code)
+        if response.status_code == 200:
+            return response
+        else:
+            print(response.json())
+    else:
+        return json.dumps({'error': 'wrong request was sended',
+                           'status': 400})
 
 
 if __name__ == '__main__':
